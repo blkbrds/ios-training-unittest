0c629103cd94dc0ab3d721463b68f1accc5e857e
Thuy Nguyen T.H <thuy.nguyen@asiantech.vn>
2020-11-04 16:11:27 +0700

    Initial Commit

---
+++ DemoUnitTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DemoUnitTest
+//
+//  Created by Thuy Nguyen T.H on 11/4/20.
+//  Copyright © 2020 thuynguyen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DemoUnitTestTests/DemoUnitTestTests.swift
@@ -0,0 +1,34 @@
+//
+//  DemoUnitTestTests.swift
+//  DemoUnitTestTests
+//
+//  Created by Thuy Nguyen T.H on 11/4/20.
+//  Copyright © 2020 thuynguyen. All rights reserved.
+//
+
+import XCTest
+@testable import DemoUnitTest
+
+class DemoUnitTestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
