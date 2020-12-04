1a020ec9ce33606b2343eecad3f1d0717a09c66b
Phat Nguyen D <phat.nguyen@monstar-lab.com>
2020-12-04 11:47:56 +0700

    fix descriptions

--- DemoUnitTestTests/ViewModel/CatCollectionViewModelTest.swift
+++ DemoUnitTestTests/ViewModel/CatCollectionViewModelTest.swift
@@ -22,33 +22,33 @@ final class CatCollectionViewModelTest: QuickSpec {
                 viewModel = CatCollectionViewModel()
             }
             describe("table view extension") {
-                it("number Of Sections") {
+                it("number of sections return 1") {
                     expect(viewModel.numberOfSections()) == 1
                 }
 
-                it("number Of Items when Items empty") {
+                it("number of items when items is empty") {
                     viewModel.cats = []
                     expect(viewModel.numberOfItems(inSection: 0)) == 0
                 }
 
-                it("number Of Items when Items has value") {
+                it("number of items when items has one value") {
                     viewModel.cats = [Cat()]
                     expect(viewModel.numberOfItems(inSection: 0)) == 1
                 }
 
-                it ("view Model For Item throw exception") {
+                it ("view model for item throw exception") {
                     let indexPath = IndexPath(row: 0, section: 0)
                     viewModel.cats = []
                     expect{ try viewModel.viewModelForItem(at: indexPath)}.to(throwError())
                 }
 
-                it ("view Model For Item no throw exception") {
+                it ("view model for item no throw exception when row less than number of cats") {
                     let indexPath = IndexPath(row: 0, section: 0)
                     viewModel.cats = [Cat()]
                     expect{ try viewModel.viewModelForItem(at: indexPath)}.toNot(throwError())
                 }
 
-                it ("view Model For Item return value") {
+                it ("view model for item return value when have view model of cat cell") {
                     let indexPath = IndexPath(row: 0, section: 0)
                     let cat1 = Cat()
                     cat1.id = "1"
