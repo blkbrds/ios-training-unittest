708643582815c5879862f67c6f4e26652308b4f8
An Nguyen T <an.nguyen2@monstar-lab.com>
2020-12-05 10:25:01 +0700

    Revert "Session 1 of unit test"
    This reverts commit 7355d14a41b7cd516b5c0c906f5c318c84096a2f.

--- DemoUnitTestTests/ViewModel/LoginViewModelTest.swift
+++ DemoUnitTestTests/ViewModel/LoginViewModelTest.swift
@@ -1,106 +1,13 @@
 //
-//  ExampleTest.swift
+//  LoginViewModelTest.swift
 //  DemoUnitTestTests
 //
-//  Created by MBA0283F on 12/2/20.
+//  Created by Thuy Nguyen T.H on 11/5/20.
 //  Copyright © 2020 thuynguyen. All rights reserved.
 //
 
-import Quick
-import Nimble
+import XCTest
 
-@testable import DemoUnitTest
-
-class LoginViewModelTest: QuickSpec {
-
-    override func spec() {
-        var viewModel: LoginViewModel!
-        context("Chấp nhận") {
-            beforeEach {
-                viewModel = LoginViewModel(name: "An", passWord: "12345678")
-            }
-            
-            describe("Chấp nhận mật khẩu") {
-                
-                it("Tên chứa chữ hoa + thường") {                    expect(viewModel.isValidUser) == true
-                }
-                
-                it("Tên chỉ chứa chữ hoa") {
-                    viewModel.name = "AAA"
-                    expect(viewModel.isValidUser) == true
-                }
-                
-                it("Tên chỉ chứa chữ thường") {
-                    viewModel.name = "aaa"
-                    expect(viewModel.isValidUser) == true
-                }
-            }
-
-            describe("Chấp nhận tên") {
-                
-                it("Mật khẩu có 8 kí tự, ko có kí tự đặc biệt") {
-                    expect(viewModel.isValidUser) == true
-                }
-                
-                it("Mật khẩu có 9 kí tự, ko có kí tự đặc biệt") {
-                    viewModel.passWord = "thseh1233"
-                    expect(viewModel.isValidUser) == true
-                }
-            }
-
-            afterEach {
-                viewModel = nil
-            }
-        }
-        
-        context("Không chấp nhận") {
-            beforeEach {
-                viewModel = LoginViewModel(name: "Ana", passWord: "12345678")
-            }
-            
-            describe("Không chấp nhận tên") {
-                it("Tên chứa kí tự số") {
-                    viewModel.name = "12CBS"
-                    expect(viewModel.isValidUser) == false
-                }
-                
-                it("Tên chứa kí tự đặc biệt") {
-                    viewModel.name = "12CBS"
-                    expect(viewModel.isValidUser) == false
-                }
-                
-                it("Tên chứa kí tự số, tự đặc biệ") {
-                    viewModel.name = "12CBS&^(@"
-                    expect(viewModel.isValidUser) == false
-                }
-            }
-            
-            describe("Không chấp nhận mật khẩu") {
-                it("Mật khẩu có 7 kí tự") {
-                    viewModel.passWord = "1234576"
-                    expect(viewModel.isValidUser) == false
-                }
-                
-                it("Mật khẩu có kí tự đặc biệt") {
-                    viewModel.passWord = "1234576@$$"
-                    expect(viewModel.isValidUser) == false
-                }
-                
-                it("Mật khẩu có 7 kí tự và có kí tự đặc biệt") {
-                    viewModel.passWord = "1234@$$"
-                    expect(viewModel.isValidUser) == false
-                }
-            }
-            
-            describe("Không chấp nhận cả hai") {
-                //
-            }
-            afterEach {
-                viewModel = nil
-            }
-        }
-        
-        
-    }
-
+class LoginViewModelTest: XCTestCase {
+    
 }
