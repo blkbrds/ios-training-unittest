6de7576b36e08520a353530f9f3d10fd418459e4
Ha Van H.N <ha.van@monstar-lab.com>
2020-12-07 11:39:17 +0700

    Setup pod install and write unit test for CatCollectionViewController

--- DemoUnitTestTests/ViewModel/InputInfoViewModelTest.swift
+++ DemoUnitTestTests/ViewModel/InputInfoViewModelTest.swift
@@ -12,28 +12,27 @@ import Quick
 
 @testable import DemoUnitTest
 
-final class InputInfoViewModelTest: QuickSpec {
-
+class TestNghiaVuQuanSu: QuickSpec {
     override func spec() {
         var viewModel: InputInfoViewModel!
 
-        context("Đủ điều kiện nhập nghĩa vụ quân sự") {
-            beforeEach() {
+        context("Đậu nghĩa vụ quân sự") {
+            beforeEach {
                 viewModel = InputInfoViewModel()
             }
-            describe("Test với tất cả input tạch nghĩa vụ quân sự") {
-                it("Đủ tuổi, không cận") {
-                    viewModel.age = 20
-                    viewModel.nearsightedness = 0
-                    expect(viewModel.validInfo()) == true
-                }
-
+            describe("Test tất cả các trường hợp đậu nghĩa vụ quân sự") {
                 it("Đủ tuổi, xém cận") {
-                    viewModel.age = 20
+                    viewModel.age = 18
                     viewModel.nearsightedness = 1.4
                     expect(viewModel.validInfo()) == true
                 }
-
+                
+                it("Xém hết tuổi, không cận") {
+                    viewModel.age = 27
+                    viewModel.nearsightedness = 0
+                    expect(viewModel.validInfo()) == true
+                }
+                
                 it("Xém hết tuổi, xém cận") {
                     viewModel.age = 27
                     viewModel.nearsightedness = 1.4
@@ -43,36 +42,40 @@ final class InputInfoViewModelTest: QuickSpec {
             afterEach {
                 viewModel = nil
             }
-
         }
-
-        context("Không đủ điều kiện nhập nghĩa vụ quân sự") {
-            beforeEach {
-                viewModel = InputInfoViewModel()
-            }
-
-            it("Hết tuổi, xém cận") {
-                viewModel.age = 29
-                viewModel.nearsightedness = 1.4
-                expect(viewModel.validInfo()) == false
-            }
-
-            it("Chưa đủ tuổi, xém cận") {
-                viewModel.age = 16
-                viewModel.nearsightedness = 1.4
-                expect(viewModel.validInfo()) == false
-            }
-
-            it("Xém hết tuổi, cận") {
-                viewModel.age = 27
-                viewModel.nearsightedness = 1.6
-                expect(viewModel.validInfo()) == false
-            }
-
-            afterEach {
-                viewModel = nil
+        
+        context("Rớt nghĩa vụ quân sự") {
+                beforeEach {
+                    viewModel = InputInfoViewModel()
+                }
+            describe("Tất cả các trường hợp rớt nghĩa vụ quân sự") {
+                it("Xém đủ tuổi, xém cận") {
+                    viewModel.age = 17
+                    viewModel.nearsightedness = 1.4
+                    expect(viewModel.validInfo()) == false
+                }
+                
+                it("Xém đủ tuổi, cận") {
+                    viewModel.age = 17
+                    viewModel.nearsightedness = 1.5
+                    expect(viewModel.validInfo()) == false
+                }
+                it("Đủ tuổi, cận") {
+                    viewModel.age = 18
+                    viewModel.nearsightedness = 1.5
+                    expect(viewModel.validInfo()) == false
+                }
+                it("Quá tuổi, xén cận") {
+                    viewModel.age = 28
+                    viewModel.nearsightedness = 1.4
+                    expect(viewModel.validInfo()) == false
+                }
+                it("Quá tuổi, cận") {
+                    viewModel.age = 28
+                    viewModel.nearsightedness = 1.5
+                    expect(viewModel.validInfo()) == false
+                }
             }
         }
-
     }
 }
